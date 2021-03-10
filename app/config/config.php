b3490986f61b050d53b7f242810d4e109e8db842
jmusyoki <jmusyoki@wastehero.io>
2021-03-10 11:37:51 +0100

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// DB Params
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', '');
+
+// Site Name
+define('SITENAME', 'SharePosts');
+
+// App Version
+define('APPVERSION', '1.0.0');
---
+++ app/views/inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+  <title><?php echo SITENAME ?></title>
+</head>
+<body>
+  <?php require APPROOT . '/views/inc/navbar.php'; ?>
+<div class="container">
